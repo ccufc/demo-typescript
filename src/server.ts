88a4f3944d3c123c533d0bde335f96cf76a7412c
EmanoelVitor <emanuelvitor575@gmail.com>
2024-08-06 14:50:11 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+type User = {
+  name: string,
+  age: number
+  email: string
+  telefone: string
+}
+
+const user: User = {
+  name: 'John Doe',
+  age: 38,
+  email: 'john.doe@example.com',
+  telefone: '(11) 9999-9999'
+}
+
+console.log(user);
